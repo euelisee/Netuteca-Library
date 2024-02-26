ebefb4735ef3c75c92827373c766c4ff2c8d1664
Elise Oliveira <euelisee@gmail.com>
2024-02-25 22:50:24 -0300

    inclusao

---
+++ src/test/java/com/example/netuteca/NetutecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netuteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class NetutecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
